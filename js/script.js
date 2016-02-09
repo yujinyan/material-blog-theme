55a456ec5786afa6511583259e043289a8b6220f
Jinyan Yu <yujinyan@outlook.com>
2016-02-09 23:47:57 +0800

    second release
    增加特色图像
    卡片样式优化
    增加卡片伸缩效果
    导航栏状态标识

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@
  */
 $(function() {
     console.log('hello world');
+
+    /* handheld menu toggle */
     $('nav .menu-toggle').click(function() {
         $(this).toggleClass('is-active');
         $('.widget-area').toggleClass('show');
         $('.site-content').toggleClass('dimmed');
     });
 
+    /* Wechat info display button */
     $('#wechat-button').click(function(e) {
         e.preventDefault();
         var imgLink = 'mywechat.jpg';
@@ -21,7 +24,50 @@ $(function() {
             });
 
         });
+    });
+
+    /* fold posts */
+    if(!$('body').hasClass('single-post')) {
+        $('.entry-content').each(function() {
+            var height = $(this).height();
+            if (height>300) {
+                $(this).parents('article.post').addClass('collapsed');
+            }
+            var that = $(this);
+            var collapsed = true;
+            $(this).parent().next().find('.continue-reading').on('click', function() {
+                console.log(collapsed);
+                collapsed = toggleCard(that, $(this), height, collapsed);
+            });
+
+            function toggleCard(elem, button, fullHeight, collapsed) {
+                if (collapsed) {
+                    elem.animate({'height': fullHeight});
+                    button.text('收起卡片');
+                    collapsed = false;
+                } else {
+                    elem.animate({'height': 300});
+                    button.text('继续阅读');
+                    collapsed = true;
+                }
+                return collapsed;
+            }
+
+        });
+    }
+
+    /* Menu border bottom */
+    var pages = ['placeholder','work','about'];
+    var pathName = document.location.pathname;
+    for (var i = 0; i < pages.length; i++) {
+        if (pathName.search(pages[i]) > -1) {
+            $('#primary-menu li a').removeClass('active');
+            $('#primary-menu li a').eq(i).addClass('active');
+            break;
+        }
+        $('#primary-menu li a').eq(0).addClass('active');
+    }
 
 
-    })
+
 });
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,45 +10,53 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
+	<?php if( has_post_thumbnail() ) {
+		echo '<div class="feature-image" style="background-image: url(' ;
+		echo the_post_thumbnail_url();
+		echo ')"></div>';
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php material_blog_posted_on(); ?>
-			<?php material_blog_entry_footer(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+	} ?>
+	<div class="entry-wrapper">
+		<header class="entry-header">
+			<?php
+				if ( is_single() ) {
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				} else {
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				}
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'material-blog' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+			if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php material_blog_posted_on(); ?>
+				<?php material_blog_entry_footer(); ?>
+			</div><!-- .entry-meta -->
+			<?php
+			endif; ?>
+		</header><!-- .entry-header -->
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'material-blog' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		<div class="entry-content">
+			<?php
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'material-blog' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
 
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'material-blog' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+	</div><!-- .entry-wrapper -->
 	<footer class="entry-footer">
 		<?php
 			if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-			echo '<span class="comments-link">';
-			comments_popup_link( esc_html__( '评论', 'material-blog' ), esc_html__( '1 个评论', 'material-blog' ), esc_html__( '% 个评论', 'material-blog' ) );
-			echo '</span>';
-		}?>
+				echo '<span class="comments-link">';
+				comments_popup_link( esc_html__( '评论', 'material-blog' ), esc_html__( '1 个评论', 'material-blog' ), esc_html__( '% 个评论', 'material-blog' ) );
+				echo '</span>';
+				echo '<span class="continue-reading">继续阅读</span>';
+			}?>
 		<?php /*material_blog_entry_footer(); */?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
